860cc69024014be7c6058e576761fc8ff6bd8fb1
Olov <olov.sundstroem@nytimes.com>
2019-10-23 12:51:19 -0400

    remove times example

--- examples/react-app/src/createApolloLink.js
+++
@@ -1,4 +0,0 @@
-import { createHttpLink } from "apollo-link-http";
-import apolloLink from "artemis";
-
-export default opts => apolloLink(createHttpLink(opts));
--- examples/react-app/src/createArtemisLink.js
+++
@@ -1,3 +0,0 @@
-import { executor } from "artemis";
-
-export default opts => executor(opts);
