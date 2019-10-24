a115f7640431d515a1b095cc7194a6ee71f65ec3
Olov <olov.sundstroem@nytimes.com>
2019-10-23 21:00:28 -0400

    Update index.js

--- src/runtime/index.js
+++ src/runtime/index.js
@@ -2,34 +2,13 @@
  * ARTEMIS PUBLIC API
  */
 
-/**
- * Usage
- * let client = createClient({ link: createLink({ uri: 'http://my-awesom-server.com/graphql' }) })
- * let query = gql`
- *  query ArticleQuery($articleId: String!) {
- *        anyWork(id: $articleId) {
- *           __typename
- *       }
- *   }
- * `
- *
- * let App = (
- *      <ArtemisProvider client={client}>
- *
- *      </ArtemisProvider>
- * )
- *
- *
- *
- */
-
 export { createClient } from "./client";
 export { useQuery, ArtemisProvider } from "./react-api";
 export { gql } from "./tag";
 export { executor } from "./link";
 
 /**
- * for the dustbin of history:
+ * on the dustbin of history:
  * hoc, render-props, decorator
  */
 export { withApollo, Query, compose, graphql } from "./react-api-legacy";
